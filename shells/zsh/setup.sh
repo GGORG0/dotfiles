3897f432d594ab76b3ba72b98d6c4aece97c8c07
GGORG <ggorg@onet.pl>
2022-03-03 17:41:00 +0100

    added zsh-you-should-use

--- shells/zsh/setup.sh
+++ shells/zsh/setup.sh
@@ -19,6 +19,7 @@ git clone --depth=1 https://github.com/kevinywlui/zlong_alert.zsh.git ${ZSH_CUST
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/jeffreytse/zsh-vi-mode.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-vi-mode
+git clone --depth=1 https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-you-should-use
 
 mv ~/.zshrc ~/.zshrc.bak
 mv ~/.p10k.zsh ~/.p10k.zsh.bak
